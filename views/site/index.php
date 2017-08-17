ed5092331d5f96c5b84a3beb66908bf831a59e6b
reutov <reutov.ilya@gmail.com>
2017-08-17 19:32:27 +0300

    Add index view

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,36 @@
 <?php
-
-/* @var $this yii\web\View */
-
-$this->title = 'My Yii Application';
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\bootstrap\Alert;
+use yii\widgets\Pjax;
+use yii\web\View;
+$this->title = "Бот Вконтакте";
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
+    <div class="container">
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            <div class="col-lg-offset-4 col-sm-offset-4 col-md-offset-4 col-xs-offset-2 col-md-12 col-sm-12 col-xs-12"> 
+                <h1>Vk Answer Bot</h1>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="col-lg-offset-4 col-sm-offset-4 col-md-offset-4 col-xs-offset-2 col-md-12 col-sm-12 col-xs-12"> 
+                <i><h4>Простой бот который <br> позволяет найти все записи на стене, <br>содержащие ключевое слово, <br>и написать к ним коментарий.</h4></i>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-lg-offset-3 col-md-offset-3 col-sm-offset-3 col-xs-12 col-lg-6 col-md-8 col-sm-8">
+                    <?php $form = ActiveForm::begin(['id' => 'bot-form']); ?>
+                    
+                    <?= $form->field($model, 'userId')->textInput()->label('ID Пользователя') ?>
+                    
+                    <?= $form->field($model, 'query')->textInput()->label('Ключевое слово') ?>
+                    
+                    <?= $form->field($model, 'comment')->textarea(['rows' => 6])?>
+                    
+                    <div class="form-group">
+                        <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary', 'name' => 'send-button']) ?>
+                    </div>
+                    
+                    <?php ActiveForm::end(); ?>
+                    <b><?php print_r($result) ?></b>
             </div>
         </div>
-
     </div>
 </div>
